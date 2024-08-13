648fd98820f9621a6971d6a8ac14227e9e8c63ce
Simon Vargas <simonvargas@simon.attlocal.net>
2024-08-13 15:10:34 -0400

    changes

--- Portolio/src/Components/Splash/Splash.js
+++ Portolio/src/Components/Splash/Splash.js
@@ -22,9 +22,8 @@ function Splash() {
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 1, ease: "easeOut" }}
     >
-      
       <div ref={homeRef} id="home" className={styles.splitSection}>
-      <div className={styles.topColorBar}></div>
+        <div className={styles.topColorBar}></div>
         <div className={styles.splitSectionContent}>
           <div className={styles.splitSectionText}>
             <p>
@@ -45,7 +44,37 @@ function Splash() {
           <p className={styles.solutionsEngineer}>Solutions Engineer</p>
         </div>
         <div class={styles.scrollButton}>
-          <span onClick={scrollToAboutMe} class={styles.arrow}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            class={styles.arrow}
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+            onClick={scrollToAboutMe}
+          >
+            <g fill="none" stroke="currentColor" stroke-linejoin="round">
+              <path d="M6 9a6 6 0 0 1 12 0v6a6 6 0 0 1-12 0z"></path>
+              <path stroke-linecap="round" d="M12 7v4"></path>
+            </g>
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            role="img"
+            class={styles.arrow2}
+            width="1em"
+            height="1em"
+            viewBox="0 0 256 256"
+            onClick={scrollToAboutMe}
+          >
+            <path
+              fill="currentColor"
+              d="M210.83 133.17a4 4 0 0 1 0 5.66l-80 80a4 4 0 0 1-5.66 0l-80-80a4 4 0 0 1 5.66-5.66L128 210.34l77.17-77.17a4 4 0 0 1 5.66 0m-85.66 5.66a4 4 0 0 0 5.66 0l80-80a4 4 0 1 0-5.66-5.66L128 130.34L50.83 53.17a4 4 0 0 0-5.66 5.66Z"
+            ></path>
+          </svg>
+          <span onClick={scrollToAboutMe} class={styles.arrow3}>
             &#8595;
           </span>
         </div>
