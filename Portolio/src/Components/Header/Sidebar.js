1369f6109d403b221527377b27507489c432edab
Simon Vargas <simonvargas@simon.attlocal.net>
2024-08-14 19:47:22 -0400

    changes

--- Portolio/src/Components/Header/Sidebar.js
+++ Portolio/src/Components/Header/Sidebar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useContext } from "react";
 import styles from "./sidebar.module.css";
 import { ScrollContext } from "../../Context/Context";
+import { motion } from 'framer-motion';
+
 
 
 function Sidebar() {
@@ -61,48 +63,48 @@ function Sidebar() {
         }
       };
 
-    return (
-    <div className={styles.sidebar1}>
-      <a
-        className={`${styles.links} ${activeLink === "home" ? styles.active : ""}`}
-        onClick={() => scrollToSection(homeRef, "home")}
-      >
-        Home
-      </a>
-      <a
-        className={`${styles.links} ${activeLink === "aboutMe" ? styles.active : ""}`}
-        onClick={() => scrollToSection(aboutMeRef, "aboutMe")}
-      >
-        About
-      </a>
-      <a
-        className={`${styles.links} ${activeLink === "projects" ? styles.active : ""}`}
-        onClick={() => scrollToSection(projectsRef, "projects")}
-      >
-        Projects
-      </a>
-      <a
-        className={`${styles.links} ${activeLink === "technologies" ? styles.active : ""}`}
-        onClick={() => scrollToSection(technologiesRef, "technologies")}
-      >
-        Technologies
-      </a>
-      <a
-        className={`${styles.links}  ${activeLink === "contact" ? styles.active : ""}`}
-        onClick={() => scrollToSection(contactRef, "contact")}
-      >
-        Contact
-      </a>
-      {/* <a
-        target="_blank"
-        rel="noreferrer"
-        href="https://docs.google.com/document/d/1j7f1ArS_JaqL84lDpetM_cpL792jFoym/edit"
-        className={styles.links}
-      >
-        Resume
-      </a> */}
-    </div>
-    )
-}
+
+      const itemVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: (i) => ({
+          opacity: 1,
+          y: 0,
+          transition: {
+            delay: i * 0.2,
+            type: "spring",
+            stiffness: 300,
+            damping: 20,
+          },
+        }),
+      };
+
+      const links = [
+        { label: "Home", ref: homeRef, section: "home" },
+        { label: "About", ref: aboutMeRef, section: "aboutMe" },
+        { label: "Projects", ref: projectsRef, section: "projects" },
+        { label: "Technologies", ref: technologiesRef, section: "technologies" },
+        { label: "Contact", ref: contactRef, section: "contact" },
+      ];
+
+      return (
+        <div className={styles.sidebar1}>
+          {links.map((link, index) => (
+            <motion.a
+              key={link.section}
+              className={`${styles.links} ${
+                activeLink === link.section ? styles.active : ""
+              }`}
+              onClick={() => scrollToSection(link.ref, link.section)}
+              custom={index}
+              initial="hidden"
+              animate="visible"
+              variants={itemVariants}
+            >
+              {link.label}
+            </motion.a>
+          ))}
+        </div>
+      );
+    }
 
 export default Sidebar;
